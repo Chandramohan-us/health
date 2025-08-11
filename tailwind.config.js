17aa3a815d579a8f9de577155f67c74fc3d9a16f
Chandramohan-us <chandramohanm2005@gmail.com>
2025-08-11 18:28:01 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neonBlue: "#00f7ff",
+        neonPink: "#ff00f7",
+      },
+    },
+  },
+  plugins: [],
+};
